fab32021a872af1ad42729a8650a2e3a8c77a79e
Christopher Bahinting <bahintingchristopher@gmail.com>
2025-09-12 15:34:48 +0800

    week2 responsive layout

---
+++ scripts/temples.js
@@ -0,0 +1,30 @@
+const menuButton = document.getElementById("menu");
+const nav = document.querySelector("nav");
+
+menuButton.addEventListener("click", () => {
+menuButton.classList.toggle("open"); // Toggle hamburger icon (☰/❌)
+nav.classList.toggle("open");        // Toggle visibility of nav
+});
+
+// in getting the current year
+const currentYear = new Date().getFullYear();
+
+//in setting modification date of the document
+const modification =document.lastModified //the lastModified was (<p id="lastModified"></p>) in footer of HTML
+
+//Now the program for the footer content to appear
+document.addEventListener("DOMContentLoaded", () => { //do this '=>'
+
+//first <p> in the footer,it is the copy right and name and location
+const copyrightP = document.querySelector("footer p:first-of-type");
+if (copyrightP) {
+copyrightP.innerHTML = `&copy; ${currentYear} | Christopher T. Bahinting | Philippines`; 
+}
+
+//second <p> in the footer 
+const lastModifiedP = document.getElementById("lastModified");
+if (lastModifiedP) {
+lastModifiedP.textContent = `Last Modification: ${modification}`; 
+}
+
+});
